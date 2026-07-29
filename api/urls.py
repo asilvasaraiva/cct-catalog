251c290be66a315f4548089d1c40398db3badbd1
Alexsandro saraiva <alex_saraiva14@hotmail.com>
2026-07-29 18:03:55 +0100

    Added  CRUD endpoints  with migrations schema

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,19 @@
 from django.urls import re_path #re_path is used for regex-based URL patterns
 
-from .views import book_view
+from .views import (
+    book_create_view,
+    book_delete_view,
+    book_list_view,
+    book_retrieve_view,
+    book_update_view,
+)
 
 app_name = "api"
 
 urlpatterns = [
-    re_path(r'books/$', book_view, name='books'),
+    re_path(r'books/$', book_list_view, name='books-list'),
+    re_path(r'books/create/$', book_create_view, name='books-create'),
+    re_path(r'books/(?P<id>\d+)/$', book_retrieve_view, name='books-retrieve'),
+    re_path(r'books/(?P<id>\d+)/update/$', book_update_view, name='books-update'),
+    re_path(r'books/(?P<id>\d+)/delete/$', book_delete_view, name='books-delete'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,68 @@
-from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# View for the api/books
+from .exceptions import BookNotFoundError
+from .serializers import BookSerializer
+from .services import BookService
 
-class BookView(APIView):
- 
+class BookListView(APIView):
     def get(self, request, *args, **kwargs):
-        return Response({"hello": "Django"})
-    
+        books = BookService.list_books()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
 
-book_view = BookView.as_view()
+
+class BookCreateView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = BookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        book = BookService.create_book(serializer.validated_data)
+        return Response(BookSerializer(book).data, status=status.HTTP_201_CREATED)
+
+
+class BookRetrieveView(APIView):
+    def get(self, request, id, *args, **kwargs):
+        try:
+            book = BookService.get_book(id)
+        except BookNotFoundError as exc:
+            raise Http404(str(exc)) from exc
+        return Response(BookSerializer(book).data)
+
+
+class BookUpdateView(APIView):
+    def _update(self, request, id, partial):
+        try:
+            book = BookService.get_book(id)
+        except BookNotFoundError as exc:
+            raise Http404(str(exc)) from exc
+
+        serializer = BookSerializer(book, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        updated_book = BookService.update_book(book, serializer.validated_data)
+        return Response(BookSerializer(updated_book).data)
+
+    def put(self, request, id, *args, **kwargs):
+        return self._update(request, id, partial=False)
+
+    def patch(self, request, id, *args, **kwargs):
+        return self._update(request, id, partial=True)
+
+
+class BookDeleteView(APIView):
+    def delete(self, request, id, *args, **kwargs):
+        try:
+            book = BookService.get_book(id)
+        except BookNotFoundError as exc:
+            raise Http404(str(exc)) from exc
+
+        BookService.delete_book(book)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+book_list_view = BookListView.as_view()
+book_create_view = BookCreateView.as_view()
+book_retrieve_view = BookRetrieveView.as_view()
+book_update_view = BookUpdateView.as_view()
+book_delete_view = BookDeleteView.as_view()
