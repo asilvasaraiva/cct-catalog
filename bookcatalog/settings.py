b90ee71fe18c3da38e6f075774e1e5bf7b8a139c
Alexsandro saraiva <alex_saraiva14@hotmail.com>
2026-08-03 14:32:38 +0100

    feat: added Swagger/OpenAPI docs via drf-spectacular to generate an OpenAPI 3 schema and serve interactive docs at /api/schema/, /api/docs/ (Swagger UI) and /api/redoc/ (Redoc).

--- bookcatalog/settings.py
+++ bookcatalog/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'api'
 ]
 
@@ -134,3 +135,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'CCT Catalog API',
+    'DESCRIPTION': 'REST API for the book catalog service.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
